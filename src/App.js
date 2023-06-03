1506dfc504dadb54ec83f4edf8917292588b179d
AnaCastroC <ana.castrocam@gmail.com>
2023-06-03 17:09:18 -0400

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Nav from './components/Navbar';
+import Home from './views/Home';
+import NotFound from './views/NotFound';
+import Contacto from './views/Contacto';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+       <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/contacto" element={<Contacto />}></Route>
+          <Route path="*" element={<NotFound/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
